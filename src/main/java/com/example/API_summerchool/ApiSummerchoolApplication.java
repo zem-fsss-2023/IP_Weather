fe50b764882f5511957887a750122d4a28ca58a6
UrhPercic <urh.percic12@gmail.com>
2023-07-11 16:04:45 +0200

    initial commit

---
+++ src/main/java/com/example/API_summerchool/ApiSummerchoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.API_summerchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSummerchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSummerchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/API_summerchool/ApiSummerchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.API_summerchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSummerchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
